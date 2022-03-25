6669b10b447ff594aeef149757ef6d8a265e7129
Mark Buchanan <gr_401@outlook.com>
2022-03-25 17:26:20 -0400

    add ve menu work

--- create_sm_csv.py
+++ create_sm_csv.py
@@ -4,24 +4,44 @@ import getpass
 #  Function definitions
 
 
-def add_ve():
-    print("Adding VE")
+def print_menu(menu_choices):
+    print("Please choose a Menu option")
+    print("-" * 25)
+    for key, value in menu_choices.items():
+        # print(key, value, sep=": ")
+        print(f"{key}: {value}")
+    print("0: To exit program")
+
+
+def get_ves():
+    print("Adding VEs")
     ve_list = []
     for i in range(3):
         ve = input(f"Please enter VE {i+1}'s callsign > ")
-        choice = input(f"You entered {ve.upper()}. Is that correct? Y/N > ")
+        choice = input(f"You entered {ve.upper()}. Is that correct? Y/N, Press C to Cancel > ")
         if choice.upper() == "Y":
             ve_list.append(ve.upper())
+        elif choice.upper() == "C":
+            print("Exiting to Main Menu ")
+            break
         else:
             ve = input(f"Please re-enter VE {i + 1}'s callsign: ")
-            choice = input(f"You entered {ve.upper()}. Is that correct? Y/N >")
+            choice = input(f"You entered {ve.upper()}. Is that correct? Y/N, Press C to Cancel > ")
             if choice.upper() == "Y":
                 ve_list.append(ve.upper())
+            elif choice.upper() == "C":
+                print("Exiting to Main Menu ")
+                break
             else:
-                print("Exiting program.  Please try again. >")
+                print("Exiting to Main Menu")
+                break
 
     #  print(f"list contains: {ve_list}")
     return tuple(ve_list)
+
+
+def get_mail_server():
+    print("Get Mail Server address")
 
 
 def get_login_creds():
@@ -34,7 +54,7 @@ def get_login_creds():
 
 
 def create_csv():
-    print("creating csv")
+    print("create csv")
 
 
 #  Main Menu
@@ -43,8 +63,9 @@ current_choice = None
 # will use "0" as exit
 available_choices = {
     "1": "Add Signing VEs",
-    "2": "Enter Login Credentials",
-    "3": "Get Registered users and Create CSV",
+    "2": "Enter Mail Server",
+    "3": "Enter Login Credentials",
+    "4": "Get Registered users and Create CSV",
 }
 
 while current_choice != "0":
@@ -52,21 +73,21 @@ while current_choice != "0":
     if current_choice in available_choices:
         print(f"DEBUG:: Current Choice: {current_choice}")
         if current_choice == "1":
-            ves = add_ve()
-            print(f"Tuple of VEs: {ves}")
+            ves = get_ves()
+            if len(ves) == 3:
+                print(f"Tuple of VEs: {ves}")
+            else:
+                print_menu(available_choices)
         elif current_choice == "2":
-            get_login_creds()
+            get_mail_server()
         elif current_choice == "3":
+            get_login_creds()
+        elif current_choice == "4":
             create_csv()
         else:
             print("DEBUG:: Reprint Menu.")
 
     else:
-        print("Please chose an option")
-        print("-" * 25)
-        for key, value in available_choices.items():
-            # print(key, value, sep=": ")
-            print(f"{key}: {value}")
-        print("0: To exit program")
-
+        # Display the menu
+        print_menu(available_choices)
     current_choice = input("> ")
